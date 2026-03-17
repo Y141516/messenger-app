da4581ce4721d9cc231065d9b57eb940bf55e42f
Y141516 <vaktgoplani9120@gmail.com>
2026-03-17 20:03:10 +0530

    temp

--- bot.js
+++ bot.js
@@ -4,20 +4,16 @@ const token = "8345855183:AAHHAtau8ADTX4mrg68JSeH7OHDwv-xiHc0"
 
 const bot = new TelegramBot(token, { polling: true })
 
-console.log("Bot is running...")
+console.log("Bot running...")
 
 bot.onText(/\/start/, (msg) => {
-  console.log("Received /start from:", msg.chat.id)
-
-  bot.sendMessage(msg.chat.id, 'Open Messenger App 👇', {
+  bot.sendMessage(msg.chat.id, "Click below to open app 👇", {
     reply_markup: {
       inline_keyboard: [
         [
           {
-            text: 'Open App 🚀',
-            web_app: {
-              url: 'https://messenger-app-ruddy.vercel.app'
-            }
+            text: "🚀 Open Messenger",
+            web_app: { url: "https://messenger-app-ruddy.vercel.app/" }
           }
         ]
       ]
